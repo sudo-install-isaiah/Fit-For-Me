20c5dcbf1ac9f23de86895f4c5463cada2f9b92b
isaiahiu <isaiah.iu@hotmail.com>
2022-05-13 13:26:23 -0400

    Add axios to client and rack cors for server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-    
-    </div>
-  );
+	useEffect(() => {
+		// for demo purposes, hardcoded URL
+		axios.get("http://localhost:3000/users").then(res => {
+			console.log(res);
+		});
+	}, []);
+
+	return (
+		<div className='App'>
+			<h1>Users</h1>
+		</div>
+	);
 }
 
 export default App;
