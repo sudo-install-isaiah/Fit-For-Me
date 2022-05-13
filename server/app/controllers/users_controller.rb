f76a60038dba0f1215b592d03ca39149e701ad3c
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-05-13 13:02:59 -0400

    add .env

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
-    render json: @users
-  end
+    # @users = User.all
+    # render json: @users
+
+    response = Excon.get(
+      "https://exercisedb.p.rapidapi.com/exercises",
+      headers: {
+        'X-RapidAPI-Host' => URI.parse("https://exercisedb.p.rapidapi.com/exercises").host,
+        'X-RapidAPI-Key' => 'e9a5605540msheab7371380e256ap16ac96jsn44941a55d88f'
+      }
+    )
+    return nil if response.status != 200
+    JSON.parse(response.body)
+    end
 end
