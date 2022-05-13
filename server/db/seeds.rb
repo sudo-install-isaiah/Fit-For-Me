5a394ec58a9fcc2a65c04bacb8e35efc21272938
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-05-13 11:47:08 -0400

    add route and setup for example user model

--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Create users!"
+User.destroy_all
+user_one = User.create(name: 'Will', email: 'willard@gmail.com', password: '123456789')
+user_two = User.create(name: 'Pablo', email: 'pablo@gmail.com', password: '123456789')
+user_three = User.create(name: 'James', email: 'james@gmail.com', password: '123456789')
