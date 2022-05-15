fb59fa3062bd543bc50b6e1cae2b0fe4eafd1003
isaiahiu <isaiah.iu@hotmail.com>
2022-05-15 18:18:13 -0400

    Modify routes to /api to take params

--- server/routes/apiExercises.js
+++ server/routes/apiExercises.js
@@ -2,22 +2,15 @@ const express = require("express");
 const router = express.Router();
 const { exerciseAPI, exerciseFilter } = require("../helpers/apiHelpers");
 
-router.get("/chest", (req, res) => {
-	console.log(`result is ------- `, req);
-	exerciseAPI("chest").then(response => {
-		const foo = exerciseFilter(req.query.equipment, response.data);
-		res.json(foo);
-	});
-});
-router.get("/back", (req, res) => {
-	exerciseAPI("back").then(response => {
-		res.json(response.data);
-	});
-});
-router.get("/shoulders", (req, res) => {
-	exerciseAPI("shoulders").then(response => {
-		res.json(response.data);
-	});
+router.get("/:type/:equipment", (req, res) => {
+	exerciseAPI(req.params.type)
+		.then(response => {
+			const result = exerciseFilter(req.params.equipment, response.data);
+			res.json(result);
+		})
+		.catch(err => {
+			console.log(err);
+		});
 });
 
 module.exports = router;
