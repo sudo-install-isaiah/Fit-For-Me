4d26073a50f38c527caca426f811f952c866e638
isaiahiu <isaiah.iu@hotmail.com>
2022-05-16 22:11:32 -0400

    Add routes for workout endpoints

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -4,21 +4,57 @@ module.exports = db => {
 	router.get("/", (req, res) => {
 		db.query(
 			`
-SELECT 
-workout_day_exercises.name as exercise_name,
-type, equipment, image
-FROM workouts
-JOIN workout_days ON workout_days.workout_id = workouts.id
-JOIN workout_day_exercises ON workout_day_exercises.workout_day_id = workout_days.id
-WHERE workouts.user_id = 1 AND workouts.is_current = true AND workout_days.is_current = true
-GROUP BY workouts.id, workout_days.id, workout_day_exercises.id
-ORDER BY workout_days.day, workout_day_exercises.priority, workout_day_exercises.type;
-
-    `
+			SELECT workouts.name as name,
+			workouts.is_current as current_workout
+			FROM workouts
+			WHERE workouts.user_id = 1 
+			ORDER BY workouts.id
+		`
 		).then(response => {
 			console.log(response.rows);
 			res.json(response.rows);
 		});
 	});
+
+	router.get("/days", (req, res) => {
+		db.query(
+			`
+		SELECT 
+		workout_days.day,
+		workout_day_exercises.name as exercise_name,
+		type, equipment, image
+		FROM workouts
+		JOIN workout_days ON workout_days.workout_id = workouts.id
+		JOIN workout_day_exercises ON workout_day_exercises.workout_day_id = workout_days.id
+		WHERE workouts.user_id = 1 AND workouts.is_current = true
+		GROUP BY workouts.id, workout_days.id, workout_day_exercises.id
+		ORDER BY workout_days.day, workout_day_exercises.priority, workout_day_exercises.type;
+		`
+		).then(response => {
+			console.log(response.rows);
+			res.json(response.rows);
+		});
+	});
+
+	router.get("/days/current", (req, res) => {
+		db.query(
+			`
+		SELECT 
+		workout_day_exercises.name as exercise_name,
+		type, equipment, image
+		FROM workouts
+		JOIN workout_days ON workout_days.workout_id = workouts.id
+		JOIN workout_day_exercises ON workout_day_exercises.workout_day_id = workout_days.id
+		WHERE workouts.user_id = 1 AND workouts.is_current = true AND workout_days.is_current = true
+		GROUP BY workouts.id, workout_days.id, workout_day_exercises.id
+		ORDER BY workout_days.day, workout_day_exercises.priority, workout_day_exercises.type;
+
+		`
+		).then(response => {
+			console.log(response.rows);
+			res.json(response.rows);
+		});
+	});
+
 	return router;
 };
