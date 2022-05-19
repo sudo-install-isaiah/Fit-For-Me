5d9cda08586da1de810579511aaff0db5c15283c
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-05-18 21:06:46 -0400

    add setchoice to grab all info from workout selected

--- client/src/components/bodyParts/MuscleGroup.jsx
+++ client/src/components/bodyParts/MuscleGroup.jsx
@@ -12,86 +12,86 @@ import Button from "@mui/material/Button";
 
 const example = [
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0291.gif",
-      "id": "0291",
-      "name": "dumbbell bench squat",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0291.gif",
+    id: "0291",
+    name: "dumbbell bench squat",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0295.gif",
-      "id": "0295",
-      "name": "dumbbell clean",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0295.gif",
+    id: "0295",
+    name: "dumbbell clean",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/3635.gif",
-      "id": "3635",
-      "name": "dumbbell contralateral forward lunge",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3635.gif",
+    id: "3635",
+    name: "dumbbell contralateral forward lunge",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0300.gif",
-      "id": "0300",
-      "name": "dumbbell deadlift",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0300.gif",
+    id: "0300",
+    name: "dumbbell deadlift",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/1760.gif",
-      "id": "1760",
-      "name": "dumbbell goblet squat",
-      "target": "quads"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1760.gif",
+    id: "1760",
+    name: "dumbbell goblet squat",
+    target: "quads",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0336.gif",
-      "id": "0336",
-      "name": "dumbbell lunge",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0336.gif",
+    id: "0336",
+    name: "dumbbell lunge",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0339.gif",
-      "id": "0339",
-      "name": "dumbbell lying femoral",
-      "target": "hamstrings"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0339.gif",
+    id: "0339",
+    name: "dumbbell lying femoral",
+    target: "hamstrings",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/3888.gif",
-      "id": "3888",
-      "name": "dumbbell one arm snatch",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3888.gif",
+    id: "3888",
+    name: "dumbbell one arm snatch",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0371.gif",
-      "id": "0371",
-      "name": "dumbbell plyo squat",
-      "target": "glutes"
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0371.gif",
+    id: "0371",
+    name: "dumbbell plyo squat",
+    target: "glutes",
   },
   {
-      "bodyPart": "upper legs",
-      "equipment": "dumbbell",
-      "gifUrl": "http://d205bpvrqc9yn1.cloudfront.net/0381.gif",
-      "id": "0381",
-      "name": "dumbbell rear lunge",
-      "target": "glutes"
-  }
-]
+    bodyPart: "upper legs",
+    equipment: "dumbbell",
+    gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0381.gif",
+    id: "0381",
+    name: "dumbbell rear lunge",
+    target: "glutes",
+  },
+];
 
 export default function MuscleGroup(props) {
   const [exercise, setExercise] = useState([]);
@@ -99,33 +99,49 @@ export default function MuscleGroup(props) {
   const equipment = props.equipment;
   const bodyPart = props.muscle;
 
-  
+  // checks the target value to the exercise id and then logs the entire obj into the state
   const handleClick = (e) => {
-    setChoice([...choice, e.target.value])
-  }
-  console.log('choice', choice);
+    const test = exercise.filter((ex) => {
+      console.log(ex.id);
+      if (ex.id === e.target.value) {
+        console.log("res", ex);
+        //example of adding the user id to the object as well
+        ex.userID = 3;
+        return ex;
+      }
+    });
+    console.log(e.target.value);
+    setChoice((prev) => [...prev, test]);
+  };
+
+  console.log("choice", choice[0]);
   useEffect(() => {
-    setExercise(example)
-  },[])
-// useEffect(() => {
-//     axios
-//       .get(`http://localhost:8080/api/${bodyPart}/${equipment}`)
-//       .then((res) => {
-//         console.log(res.data);
-//         setExercise(res.data);
-//       });
-//   }, [equipment]);
+    setExercise(example);
+  }, []);
+  // useEffect(() => {
+  //     axios
+  //       .get(`http://localhost:8080/api/${bodyPart}/${equipment}`)
+  //       .then((res) => {
+  //         console.log(res.data);
+  //         setExercise(res.data);
+  //       });
+  //   }, [equipment]);
 
   console.log("_____", exercise);
 
   const data = exercise.map((ex, index) => {
-      const info = JSON.stringify(ex);
-    return <div>
-      <WorkoutListItem name={ex.name} image={ex.gifUrl} priority={1} type={ex.bodyPart}/>
-      <Checkbox value={info} onChange={e => handleClick(e)}/>
-      </div>;
+    const info = JSON.stringify(ex);
+    return (
+      <div>
+        <WorkoutListItem
+          name={ex.name}
+          image={ex.gifUrl}
+          priority={1}
+          type={ex.bodyPart}
+        />
+        <Checkbox value={ex.id} onChange={(e) => handleClick(e)} />
+      </div>
+    );
   });
-  return (
-   <div>{data}</div>
-  )
+  return <div>{data}</div>;
 }
