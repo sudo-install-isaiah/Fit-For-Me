d192f7b495924ff6708d9d6ace2bf37ef680019c
isaiahiu <isaiah.iu@hotmail.com>
2022-05-19 16:43:35 -0400

    Create Empty component for homepage

---
+++ client/src/components/Workout/Empty.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Empty() {
+	return (
+		<main>
+			<img
+				className='btn'
+				src='images/add.png'
+				alt='Add'
+			/>
+		</main>
+	);
+}
