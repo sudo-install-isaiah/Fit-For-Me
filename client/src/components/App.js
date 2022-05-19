a61c172aa569ae28a30860432ad13ce699e5d76d
isaiahiu <isaiah.iu@hotmail.com>
2022-05-19 17:17:49 -0400

    Create Empty component that links to /new

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,27 +1,32 @@
 import "./App.css";
-import { useState, useEffect,useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Link, Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Workout from "./Workout";
 import { UsersContext } from "./providers/UserProvider";
+import Empty from "./Empty";
 
 function App() {
-	const [state, setState] = useState([]);
-	const {cookies} = useContext(UsersContext)
+	const [workout, setWorkout] = useState([]);
+	const { cookies } = useContext(UsersContext);
 	useEffect(() => {
-		
-		axios.get(`http://localhost:8080/workouts/days/current`, { params: {id: cookies.id}}).then(res => {
-			console.log(res.data);
-			setState(res.data);
-		});
-	}, []);
-	
+		axios
+			.get(`http://localhost:8080/workouts/days/current`, {
+				params: { id: cookies.id },
+			})
+			.then(res => {
+				console.log(res.data);
+				setWorkout(res.data);
+			});
+	}, [cookies]);
+
 	return (
 		<div className='App'>
 			<Navbar></Navbar>
 			<br />
-			<Workout workout={state}></Workout>
+			<main>
+				{workout.length > 0 ? <Workout workout={workout}></Workout> : <Empty />}
+			</main>
 		</div>
 	);
 }
