c0b31fd5fb8283f1ae83417e6f681148f677f2b3
isaiahiu <isaiah.iu@hotmail.com>
2022-05-20 13:42:42 -0400

    Implement homepage button in NewWorkout

--- client/src/components/NavBarCreate.jsx
+++ client/src/components/NavBarCreate.jsx
@@ -11,8 +11,10 @@ export default function NavBarCreate(props) {
 	return (
 		<nav className='navbar'>
 			<div className='container-fluid'>
-				<span className='nav-item'>
+        <span className='nav-item'>
+          <Link to="/">
 					<ArrowBackIosNew className='logo-icon'></ArrowBackIosNew>
+          </Link>
         </span>
         <h1 className="nav-item logo">Create Workout</h1>
 				<div className='nav-item logo '>
