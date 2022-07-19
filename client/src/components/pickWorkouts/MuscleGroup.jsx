09709dbed2e9bd647ad9c2a9e214bf8642170f98
zae <isaiah.iu@hotmail.com>
2022-07-19 17:51:35 -0400

    Project Organization
    - Rename MuscleGroup to MuscleItem.
    - Create new MuscleGroup component to act as parent for MuscleItem.

--- client/src/components/pickWorkouts/MuscleGroup.jsx
+++ client/src/components/pickWorkouts/MuscleGroup.jsx
@@ -1,178 +1,29 @@
-import React, { useContext } from "react";
-import axios from "axios"; //keep
-import { useEffect, useState } from "react";
-import WorkoutListItem from "../Workout/WorkoutListItem";
+import React from "react";
 import {
-	InputLabel,
-	MenuItem,
-	FormControl,
-	Select,
-	Checkbox,
-	ListItem,
+	Accordion,
+	AccordionSummary,
+	Typography,
+	AccordionDetails,
 } from "@mui/material";
-import { WorkoutContext } from "../providers/WorkoutFormProvider";
-import './muscleGroup.css'
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MuscleItem from "./MuscleItem";
 
-// can be deleted hardcoded for testing purposes
-const example = [
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0291.gif",
-		id: "0291",
-		name: "dumbbell bench squat",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0295.gif",
-		id: "0295",
-		name: "dumbbell clean",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3635.gif",
-		id: "3635",
-		name: "dumbbell contralateral forward lunge",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0300.gif",
-		id: "0300",
-		name: "dumbbell deadlift",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1760.gif",
-		id: "1760",
-		name: "dumbbell goblet squat",
-		target: "quads",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0336.gif",
-		id: "0336",
-		name: "dumbbell lunge",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0339.gif",
-		id: "0339",
-		name: "dumbbell lying femoral",
-		target: "hamstrings",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3888.gif",
-		id: "3888",
-		name: "dumbbell one arm snatch",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0371.gif",
-		id: "0371",
-		name: "dumbbell plyo squat",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0381.gif",
-		id: "0381",
-		name: "dumbbell rear lunge",
-		target: "glutes",
-	},
-];
-
-export default function MuscleGroup(props) {
-	const { exercise, setExercise, setChoice } = useContext(WorkoutContext);
-	const [equipment, setEquipment] = useState("");
-	const bodyPart = props.muscle; // keep
-
-	// checks the target value to the exercise id and then logs the entire obj into the state
-	const handleClick = e => {
-		const test = exercise.filter(ex => {
-			if (ex.id === e.target.value) {
-				//example of adding the user id to the object as well
-				return ex;
-			}
-		});
-		// returns array of objects
-		const test1 = test[0];
-		setChoice(prev => {
-			return { ...prev, workout: [...prev.workout, test1] };
-		});
-	};
-
-	const handleChanges = event => {
-		setEquipment(event.target.value);
-	};
-
-	// for testing purposes
-	useEffect(() => {
-		setExercise(example);
-	}, []);
-
-	// real data from api
-	// useEffect(() => {
-	//     axios
-	//       .get(`http://localhost:8080/api/${bodyPart}/${equipment}`)
-	//       .then((res) => {
-	//         setExercise(res.data);
-	//       });
-	//   }, [equipment]);
-
-	const data = exercise.map((ex, index) => {
-		return (
-			<ListItem className='exercise-select' key={ex.id}>
-				<WorkoutListItem
-					key={ex.id}
-					name={ex.name}
-					image={ex.gifUrl}
-					priority={1}
-					type={ex.bodyPart}
-				></WorkoutListItem>
-				<Checkbox value={ex.id} onChange={e => handleClick(e)} />
-			</ListItem>
-		);
-	});
-
+export default function MuscleGroup({ muscle, equipment }) {
 	return (
-		<div className="muscle-group">
-			<FormControl  sx={{ m: 1, minWidth: 120 }}>
-				<InputLabel className='equipment-form' id='select-equipment-label'>
-					Equipment
-				</InputLabel>
-				<Select
-					style={{ backgroundColor: "rgb(204 164 58 / 55%)" }}
-					variant='filled'
-					id='equipment'
-					label='equipment'
-					value={equipment}
-					labelId='select-equipment-label'
-					onChange={handleChanges}
-					disableUnderline
-				>
-					<MenuItem value='barbell'>Barbell</MenuItem>
-					<MenuItem value='band'>Band</MenuItem>
-					<MenuItem value='body weight'>Body Weight</MenuItem>
-					<MenuItem value='dumbbell'>Dumbbell</MenuItem>
-				</Select>
-			</FormControl>
-			{data}
-		</div>
+		<Accordion>
+			<AccordionSummary
+				expandIcon={<ExpandMoreIcon />}
+				aria-controls='panel1a-content'
+				id='panel1a-header'
+			>
+				{" "}
+				<Typography component={"span"} align='center'>
+					{muscle}
+				</Typography>
+			</AccordionSummary>
+			<AccordionDetails>
+				<MuscleItem muscle={muscle} equipment={equipment} />
+			</AccordionDetails>
+		</Accordion>
 	);
 }
