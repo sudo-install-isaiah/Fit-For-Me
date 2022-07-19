36e2e4fd2e18cf396cb3822c937a1dbb0bd202e4
zae <isaiah.iu@hotmail.com>
2022-07-19 19:10:25 -0400

    Remove test code

--- client/src/components/pickWorkouts/MuscleItem.jsx
+++ client/src/components/pickWorkouts/MuscleItem.jsx
@@ -11,93 +11,93 @@ import {
 	ListItem,
 } from "@mui/material";
 import { WorkoutContext } from "../providers/WorkoutFormProvider";
-import './muscleGroup.css'
+import "./muscleGroup.css";
 
 // can be deleted hardcoded for testing purposes
-const example = [
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0291.gif",
-		id: "0291",
-		name: "dumbbell bench squat",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0295.gif",
-		id: "0295",
-		name: "dumbbell clean",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3635.gif",
-		id: "3635",
-		name: "dumbbell contralateral forward lunge",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0300.gif",
-		id: "0300",
-		name: "dumbbell deadlift",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1760.gif",
-		id: "1760",
-		name: "dumbbell goblet squat",
-		target: "quads",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0336.gif",
-		id: "0336",
-		name: "dumbbell lunge",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0339.gif",
-		id: "0339",
-		name: "dumbbell lying femoral",
-		target: "hamstrings",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3888.gif",
-		id: "3888",
-		name: "dumbbell one arm snatch",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0371.gif",
-		id: "0371",
-		name: "dumbbell plyo squat",
-		target: "glutes",
-	},
-	{
-		bodyPart: "upper legs",
-		equipment: "dumbbell",
-		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0381.gif",
-		id: "0381",
-		name: "dumbbell rear lunge",
-		target: "glutes",
-	},
-];
+// const example = [
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0291.gif",
+// 		id: "0291",
+// 		name: "dumbbell bench squat",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0295.gif",
+// 		id: "0295",
+// 		name: "dumbbell clean",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3635.gif",
+// 		id: "3635",
+// 		name: "dumbbell contralateral forward lunge",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0300.gif",
+// 		id: "0300",
+// 		name: "dumbbell deadlift",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/1760.gif",
+// 		id: "1760",
+// 		name: "dumbbell goblet squat",
+// 		target: "quads",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0336.gif",
+// 		id: "0336",
+// 		name: "dumbbell lunge",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0339.gif",
+// 		id: "0339",
+// 		name: "dumbbell lying femoral",
+// 		target: "hamstrings",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/3888.gif",
+// 		id: "3888",
+// 		name: "dumbbell one arm snatch",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0371.gif",
+// 		id: "0371",
+// 		name: "dumbbell plyo squat",
+// 		target: "glutes",
+// 	},
+// 	{
+// 		bodyPart: "upper legs",
+// 		equipment: "dumbbell",
+// 		gifUrl: "http://d205bpvrqc9yn1.cloudfront.net/0381.gif",
+// 		id: "0381",
+// 		name: "dumbbell rear lunge",
+// 		target: "glutes",
+// 	},
+// ];
 
-export default function MuscleItem (props) {
+export default function MuscleItem(props) {
 	const { exercise, setExercise, setChoice } = useContext(WorkoutContext);
 	const [equipment, setEquipment] = useState("");
 	const bodyPart = props.muscle; // keep
@@ -122,18 +122,18 @@ export default function MuscleItem (props) {
 	};
 
 	// for testing purposes
-	useEffect(() => {
-		setExercise(example);
-	}, []);
+	// useEffect(() => {
+	// 	setExercise(example);
+	// }, []);
 
 	// real data from api
-	// useEffect(() => {
-	//     axios
-	//       .get(`http://localhost:8080/api/${bodyPart}/${equipment}`)
-	//       .then((res) => {
-	//         setExercise(res.data);
-	//       });
-	//   }, [equipment]);
+	useEffect(() => {
+		axios
+			.get(`http://localhost:8080/api/${bodyPart}/${equipment}`)
+			.then(res => {
+				setExercise(res.data);
+			});
+	}, [equipment]);
 
 	const data = exercise.map((ex, index) => {
 		return (
@@ -151,8 +151,8 @@ export default function MuscleItem (props) {
 	});
 
 	return (
-		<div className="muscle-group">
-			<FormControl  sx={{ m: 1, minWidth: 120 }}>
+		<div className='muscle-group'>
+			<FormControl sx={{ m: 1, minWidth: 120 }}>
 				<InputLabel className='equipment-form' id='select-equipment-label'>
 					Equipment
 				</InputLabel>
